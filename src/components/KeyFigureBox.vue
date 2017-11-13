cacbd208ebd9fa29c716f7fe98541868aa0ccfc1
Niclas Olofsson <n@niclasolofsson.se>
2017-11-13 18:22:40 +0100

    Show development percentage for key values.

--- src/components/KeyFigureBox.vue
+++ src/components/KeyFigureBox.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h3>{{ title }}</h3>
-    <dl>
-      <li v-for="item in data">
-        <b>{{ item.x }}</b> {{ item.y }}
-      </li>
-    </dl>
+    <table>
+      <tr v-for="item in getItems()">
+        <th>{{ item.periodName }}</th>
+        <td>{{ item.value }}</td>
+        <td>
+          <span v-if="item.changePercent">{{ item.changePercent | formatPercent }}</span>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -13,11 +17,38 @@
 export default {
   name: 'KeyFigureBox',
   props: ['title', 'data'],
+  filters: {
+    formatPercent(value) {
+      return `${Math.round(value * 10) / 10} %`
+    }
+  },
+  methods: {
+    getItems() {
+      let previousValue = null
+
+      return this.data.map((item) => {
+        const result = {
+          periodName: item.x,
+          value: item.y,
+          changePercent: null,
+        }
+        if (previousValue) {
+          result.changePercent = ((result.value - previousValue) / previousValue) * 100
+        }
+        previousValue = result.value
+        return result
+      })
+    }
+  },
 }
 </script>
 
 <style scoped>
 div {
   margin: 10px;
+}
+td {
+  padding: 10px;
+  text-align: right;
 }
 </style>
