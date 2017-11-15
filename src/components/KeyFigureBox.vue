cbe72cd7fd3bc57b42cd44eb38ee55781786988e
Niclas Olofsson <n@niclasolofsson.se>
2017-11-15 17:55:03 +0100

    Add some sort of basic trendline support.

--- src/components/KeyFigureBox.vue
+++ src/components/KeyFigureBox.vue
@@ -2,16 +2,20 @@
   <div>
     <h3>{{ title }}</h3>
     <table>
-      <tr v-for="year in years">
-        <th>{{ year.periodName }}</th>
-        <td>{{ year.value }}</td>
+      <tr v-for="item in yearItems.slice().reverse()">
+        <th>{{ item.periodName }}</th>
+        <td>{{ item.value }}</td>
         <td>
-          <span v-if="year.changePercent">{{ year.changePercent | formatPercent }}</span>
+          <span v-if="item.changePercent">{{ item.changePercent | formatPercent }}</span>
         </td>
       </tr>
       <tr>
         <td>Medel</td>
         <td colspan="2">{{ averagePercent | formatPercent }}</td>
+      </tr>
+      <tr>
+        <td>Trendlinje</td>
+        <td colspan="2">{{ trendPercent | formatPercent }}</td>
       </tr>
       <tr>
         <td>Gissning</td>
@@ -24,6 +28,8 @@
 </template>
 
 <script>
+import regression from 'regression'
+
 export default {
   name: 'KeyFigureBox',
   props: ['title', 'data'],
@@ -33,26 +39,38 @@ export default {
     }
   },
   computed: {
-    years() {
+    yearItems() {
       let previousValue = null
 
       return this.data.map((item) => {
         const result = {
           periodName: item.x,
-          value: item.y,
+          value: parseFloat(item.y),
           changePercent: null,
+          year: parseInt(item.x.slice(-4), 10),  // Yup, this is pretty ugly.
         }
         if (previousValue) {
           result.changePercent = ((result.value - previousValue) / previousValue) * 100
         }
         previousValue = result.value
         return result
-      }).reverse()
+      })
     },
     averagePercent() {
-      const sum = this.years.reduce((sum, year) => sum + (year.changePercent || 0), 0)
-      const count = this.years.length - 1
+      const sum = this.yearItems.reduce((sum, item) => sum + (item.changePercent || 0), 0)
+      const count = this.yearItems.length - 1
       return sum / count
+    },
+    trendPercent() {
+      if (!this.yearItems.length) {
+        return null
+      }
+      const values = this.yearItems.map((item) => [item.year, item.value])
+      this.trend = regression.linear(values)
+
+      const lastItem = this.yearItems[this.yearItems.length - 1]
+      const nextValue = this.trend.predict(lastItem.year + 5)[1]
+      return ((nextValue - lastItem.value) / lastItem.value) * 100 / 5
     },
   },
 }
